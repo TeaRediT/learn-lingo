cb28055a94ea4942cc87c1ad2cf8753a7c255895
TeaRediT <vlad.bobro.02@gmail.com>
2026-06-15 17:30:58 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-family",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${roboto.variable} scroll-smooth`}>
+      <body>{children}</body>
+    </html>
+  );
+}

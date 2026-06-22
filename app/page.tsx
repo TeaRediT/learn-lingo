4724d8c6534910b712e025697338457b4386cae2
TeaRediT <vlad.bobro.02@gmail.com>
2026-06-22 21:23:53 +0300

    add main page hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
 import Container from "@/components/common/container/Container";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Container>
-      <p className="text-yellow">Hello</p>
-    </Container>
+    <main>
+      <section>
+        <Container>
+          <div className="flex justify-between">
+            <div className="w-180 rounded-[30px] bg-[#f8f8f8] py-24.5 px-16">
+              <h1 className="text-[48px] leading-[1.17] tracking-[-0.02em] mb-8">
+                Unlock your potential with the best{" "}
+                <span className="relative z-0">
+                  <span className="absolute bg-yellow-soft rounded-lg -z-10 top-3 bottom-1.5 left-0.5 right-1"></span>
+                  <em className="font-normal px-0.5">language</em>{" "}
+                </span>
+                tutors
+              </h1>
+              <p className="font-normal leading-snug  tracking-[-0.02em] w-120 mb-16">
+                Embark on an Exciting Language Journey with Expert Language
+                Tutors: Elevate your language proficiency to new heights by
+                connecting with highly qualified and experienced tutors.
+              </p>
+              <Link
+                className="leading-[1.56] font-bold text-[18px] inline-flex justify-center items-center h-15 min-w-66.75 bg-yellow rounded-xl transition-colors duration-250 [:hover,:focus]:bg-[#ffdc86]"
+                href={"/teachers"}
+              >
+                Get started
+              </Link>
+            </div>
+            <div className="relative w-142 rounded-[30px] overflow-hidden">
+              <Image
+                className="object-cover"
+                src={"/hero.png"}
+                fill
+                priority
+                sizes="568px"
+                alt="Language tutor illustration"
+              />
+            </div>
+          </div>
+        </Container>
+      </section>
+    </main>
   );
 }

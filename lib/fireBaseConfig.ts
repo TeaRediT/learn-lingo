9de9cc47d04d00fe146b9e0e5d84534eaa508685
TeaRediT <vlad.bobro.02@gmail.com>
2026-06-27 17:35:20 +0300

    update firebase config

--- lib/fireBaseConfig.ts
+++ lib/fireBaseConfig.ts
@@ -5,7 +5,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.FB_API_KEY,
   authDomain: process.env.FB_AUTH_DOMAIN,
-  databaseURL: `https://${process.env.FB_PROJECT_ID}.firebaseio.com`,
+  databaseURL: process.env.FB_DB_URL,
   projectId: process.env.FB_PROJECT_ID,
   storageBucket: process.env.FB_STORAGE_BUCKET,
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
